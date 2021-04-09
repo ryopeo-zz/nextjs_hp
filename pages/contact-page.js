a40c1ea3d1161e83d21799f532d1843d455fcef3
ryopeo <ryopeo@gmail.com>
2021-04-09 13:24:13 +0900

    change to function

--- pages/contact-page.js
+++ pages/contact-page.js
@@ -1,7 +1,7 @@
 import Layout from '../components/Layout';
 import Image from 'next/image';
 
-const Contact = () => {
+export default function Contact() {
   return (
     <Layout title='Blog'>
       <div className='bg-white text-center shadow-xl p-8 w-80 rounded'>
@@ -79,6 +79,4 @@ const Contact = () => {
       </div>
     </Layout>
   );
-};
-
-export default Contact;
+}
